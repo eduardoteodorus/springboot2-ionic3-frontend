7f52802336bd99022678f3fb48127a59e57f9549
eduardoteodorus <eduardo@vaio>
2019-02-03 19:07:07 -0200

    Pegando imagem da galeria

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://curso-springboot2-app.herokuapp.com",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-ionic-aws-s3"
 }
